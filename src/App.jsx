6a7dacb5bc22c40b043b5e74692e212888902bed
Nikhil More <moren9817@gmail.com>
2025-05-21 19:16:23 +0530

    pull req #001-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
 
+import { useState } from 'react'
+import './App.css'
 const App = () => {
+  const [color,setColor] = useState("aqua");
   return (  
     <>
     {/* react fiber github */}
+    <div className="w-full h-screen duration-200"
+    style={{backgroundColor:color}}>
+
+      <div className="absolute flex flex-wrap justify-center bottom-12  inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-4 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg "
+          style={{backgroundColor:"red"}} onClick={()=>setColor("red")}>Red</button>
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor:"green"}} onClick={()=>setColor("green")}>green</button>
+          <button className="outline-none px-4 py-1 rounded-full shadow-lg text-white"
+          style={{backgroundColor:"black"}} onClick={()=>setColor("black")}>black</button>
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" 
+          style={{backgroundColor:"blue"}} onClick={()=>setColor("blue")}> blue</button>
+        </div>
+      </div>
+    </div>
     </>
   )
 }
